8d56b1cee22db2914366491ab07001fd2a456b58
juberlandio <juberlandio.santana.js@gmail.com>
2024-06-16 10:03:12 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-const btn = document.querySelector('.btn')
-const container = document.querySelector('body')
-
-btn.onclick = function(){
-    this.classList.toggle('ativo-1')
-    container.classList.toggle('ativo-1')
-}
+function Botao() {
+    const btn = document.querySelector('.btn')
+    const container = document.querySelector('body')
+    
+    btn.onclick = function(){
+        this.classList.toggle('ativo-1')
+        container.classList.toggle('ativo-1')
+    }
+    }
+    Botao();
+    
+    
+    function clickBotao() {
+        document.addEventListener("DOMContentLoaded", function() {
+          var buttons = document.querySelectorAll(".soundButton");
+          var audio = new Audio('sons/click-sound.mp3');
+      
+          audio.volume = 0.4;
+          buttons.forEach(function(button) {
+              button.addEventListener("click", function() {
+                  audio.play();
+              });
+          });
+      });
+    }
+    clickBotao();
